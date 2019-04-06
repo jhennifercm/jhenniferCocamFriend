9f49acd2b94c052276ac29bfa2aefb88f872ff30
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-04-06 09:57:37 -0400

    comit modelo web api(El anterior era proyecto)

---
+++ JhenniferCocaFriend/APIfriend/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIfriend.Models
+{
+    public class DataContext:DbContext
+
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIfriend.Models.Friend> Friends { get; set; }
+    }
+}
---
+++ JhenniferCocaFriend/APIfriend/Models/Friend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIfriend.Models
+{
+    public class Friend
+    {
+        public enum TipoFriend {
+            Conocido,//0
+            CompaneroEstudio,//1
+            ColegaDeTrabajo,//2
+            Amigo,//3
+            AmigoDeInfancia,//4
+            Pariente//5
+        }
+        [Key]
+        public int FriendId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        public string Nickname { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        [Required]
+        public TipoFriend Type { get; set; }
+
+
+
+    }
+}
